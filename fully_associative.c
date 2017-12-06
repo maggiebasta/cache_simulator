381d9bc635e1c76d9ab882594bb6fa9dd4b1ea46
Marguerite Basta <mbasta@pixability.com>
2017-12-06 09:27:47 -0500

    finish fac

--- fully_associative.c
+++ fully_associative.c
@@ -1,5 +1,6 @@
 #include "memory_block.h"
 #include "fully_associative.h"
+#include <math.h>
 
 fully_associative_cache* fac_init(main_memory* mm)
 {
@@ -14,7 +15,7 @@ fully_associative_cache* fac_init(main_memory* mm)
         result->ways[i].block->size = MAIN_MEMORY_BLOCK_SIZE;
         result->ways[i].valid = 0;
         result->ways[i].dirty = 0;
-        result->ways[i].lru_priority = FULLY_ASSOCIATIVE_NUM_WAYS - 1;
+        result->ways[i].lru_priority = 999999999;
     }
     return result;
 }
@@ -26,7 +27,7 @@ static void mark_as_used(fully_associative_cache* fac, int way)
 	int i;
 	for(i = 0; i < FULLY_ASSOCIATIVE_NUM_WAYS; i++)
     {
-        if(fac->ways[i].valid != 0){
+        if(fac->ways[i].valid == 1){
         	fac->ways[i].lru_priority += 1;
         }
     }
@@ -35,14 +36,17 @@ static void mark_as_used(fully_associative_cache* fac, int way)
 
 static int lru(fully_associative_cache* fac)
 {
-	int i;
-	for(i = 0; i < FULLY_ASSOCIATIVE_NUM_WAYS; i++)
+    int i;
+    int maxp = -1;
+    int maxi = -1;
+    for(i = 0; i < FULLY_ASSOCIATIVE_NUM_WAYS; i++)
     {
-        if(fac->ways[i].lru_priority == FULLY_ASSOCIATIVE_NUM_WAYS - 1){
-        	return i;
+        if(fac->ways[i].lru_priority > maxp){
+            maxp = fac->ways[i].lru_priority;
+            maxi = i;
         }
     }
-    return -1;
+    return maxi;
 }
 
 
@@ -50,6 +54,8 @@ void fac_store_word(fully_associative_cache* fac, void* addr, unsigned int val)
 {
     size_t addr_offt = (size_t) (addr - MAIN_MEMORY_START_ADDR) % MAIN_MEMORY_BLOCK_SIZE;
     void* mb_start_addr = addr - addr_offt;
+
+    int lastw = lru(fac);
 
     int hit = 0;
     int idx;
@@ -65,13 +71,13 @@ void fac_store_word(fully_associative_cache* fac, void* addr, unsigned int val)
     if (hit == 1){
     	unsigned int* mb_addr = fac->ways[idx].block->data + addr_offt;
     	*mb_addr = val;
+        fac->ways[idx].dirty = 1;
     	fac->ways[idx].dirty = 1;
     	mark_as_used(fac, idx);
 
     	++fac->cs.w_queries;
     }
     else{
-    	int lastw = lru(fac);
     	if (fac->ways[lastw].valid == 1 && fac->ways[lastw].dirty == 1){
     		mm_write(fac->mm, fac->ways[lastw].block->start_addr, fac->ways[lastw].block);
     	}
@@ -95,9 +101,12 @@ unsigned int fac_load_word(fully_associative_cache* fac, void* addr)
     size_t addr_offt = (size_t) (addr - MAIN_MEMORY_START_ADDR) % MAIN_MEMORY_BLOCK_SIZE;
     void* mb_start_addr = addr - addr_offt;
 
+    int lastw = lru(fac);
+
     int hit = 0;
     int idx;
     int i;
+
     for(i = 0; i < FULLY_ASSOCIATIVE_NUM_WAYS; i++)
     {
         if((fac->ways[i].valid == 1) && (fac->ways[i].block->start_addr == mb_start_addr)){
@@ -109,16 +118,15 @@ unsigned int fac_load_word(fully_associative_cache* fac, void* addr)
     if (hit == 1){
     	unsigned int* mb_addr = fac->ways[idx].block->data + addr_offt;
     	unsigned int result = *mb_addr;
-    	fac->ways[idx].dirty = 1;
     	mark_as_used(fac, idx);
     	++fac->cs.r_queries;
     	return result;
 
     }
     else{
-    	int lastw = lru(fac);
     	if (fac->ways[lastw].valid == 1 && fac->ways[lastw].dirty == 1){
     		mm_write(fac->mm, fac->ways[lastw].block->start_addr, fac->ways[lastw].block);
+            fac->ways[lastw].dirty = 0;
     	}
     	memory_block* mb = mm_read(fac->mm, mb_start_addr);
     	unsigned int* mb_addr = mb->data + addr_offt;
--- set_associative.c
+++ set_associative.c
@@ -8,23 +8,23 @@
 set_associative_cache* sac_init(main_memory* mm)
 {
 
-	set_associative_cache* result = malloc(sizeof(set_associative_cache));
+    set_associative_cache* result = malloc(sizeof(set_associative_cache));
     result->mm = mm;
     result->cs = cs_init();
 
-	int i;
+    int i;
     int j;
     for(i = 0; i < SET_ASSOCIATIVE_NUM_SETS; i++)
     {
-	    for(j = 0; j < SET_ASSOCIATIVE_NUM_WAYS; j++)
-	    {
-	        result->sets[i].ways[j].block = malloc(sizeof(struct memory_block));
-	        result->sets[i].ways[j].block->size = MAIN_MEMORY_BLOCK_SIZE;
-	        result->sets[i].ways[j].valid = 0;
-	        result->sets[i].ways[j].dirty = 0;
-	        result->sets[i].ways[j].lru_priority = SET_ASSOCIATIVE_NUM_WAYS - 1;
-	    }
-	}
+        for(j = 0; j < SET_ASSOCIATIVE_NUM_WAYS; j++)
+        {
+            result->sets[i].ways[j].block = malloc(sizeof(struct memory_block));
+            result->sets[i].ways[j].block->size = MAIN_MEMORY_BLOCK_SIZE;
+            result->sets[i].ways[j].valid = 0;
+            result->sets[i].ways[j].dirty = 0;
+            result->sets[i].ways[j].lru_priority = 999999999;
+        }
+    }
     return result;
 }
 
@@ -43,49 +43,52 @@ static char *getBinary(unsigned int num)
 
 static int addr_to_set(void* addr)
 {
-	int intaddr = (uintptr_t)addr;
-	char* baddr = getBinary(intaddr);
+    int intaddr = (uintptr_t)addr;
+    char* baddr = getBinary(intaddr);
 
-	int setbits = log2(SET_ASSOCIATIVE_NUM_SETS);
-	int setindex = 0;
-	int startidx = 30 - setbits;
-	int i;
-	for (i = 0; i<setbits; i++){
-		int x = baddr[startidx + i] - '0';
-		setindex += x * pow(2, (setbits-i));
-	}
+    int setbits = log2(SET_ASSOCIATIVE_NUM_SETS);
+    int setindex = 0;
+    int startidx = 30 - setbits;
+    int i;
+    for (i = 0; i<setbits; i++){
+        int x = baddr[startidx + i] - '0';
+        setindex += x * pow(2, (setbits-i));
+    }
     
     return setindex;
 }
 
 static void mark_as_used(set_associative_cache* sac, int set, int way)
 {
-	int i;
-	for(i = 0; i < SET_ASSOCIATIVE_NUM_WAYS; i++)
+    int i;
+    for(i = 0; i < SET_ASSOCIATIVE_NUM_WAYS; i++)
     {
-        if(sac->sets[set].ways[i].valid != 0){
-        	sac->sets[set].ways[i].lru_priority += 1;
+        if(sac->sets[set].ways[i].valid == 1){
+            sac->sets[set].ways[i].lru_priority += 1;
         }
     }
-	sac->sets[set].ways[way].lru_priority = 0;
+    sac->sets[set].ways[way].lru_priority = 0;
 }
 
 static int lru(set_associative_cache* sac, int set)
 {
-	int i;
-	for(i = 0; i < SET_ASSOCIATIVE_NUM_WAYS; i++)
+    int i;
+    int maxp = -1;
+    int maxi = -1;
+    for(i = 0; i < SET_ASSOCIATIVE_NUM_WAYS; i++)
     {
-        if(sac->sets[set].ways[i].lru_priority == SET_ASSOCIATIVE_NUM_WAYS - 1){
-        	return i;
+        if(sac->sets[set].ways[i].lru_priority > maxp){
+            maxp = sac->sets[set].ways[i].lru_priority;
+            maxi = i;
         }
     }
-    return -1;
+    return maxi;
 }
 
 
 void sac_store_word(set_associative_cache* sac, void* addr, unsigned int val)
 {
-   	size_t addr_offt = (size_t) (addr - MAIN_MEMORY_START_ADDR) % MAIN_MEMORY_BLOCK_SIZE;
+    size_t addr_offt = (size_t) (addr - MAIN_MEMORY_START_ADDR) % MAIN_MEMORY_BLOCK_SIZE;
     void* mb_start_addr = addr - addr_offt;
 
     int setidx = addr_to_set(addr);
@@ -96,36 +99,36 @@ void sac_store_word(set_associative_cache* sac, void* addr, unsigned int val)
     for(i = 0; i < SET_ASSOCIATIVE_NUM_WAYS; i++)
     {
         if((sac->sets[setidx].ways[i].valid == 1) && (sac->sets[setidx].ways[i].block->start_addr == mb_start_addr)){
-        	hit = 1;
-        	idx = i;
+            hit = 1;
+            idx = i;
         }
     }
 
     if (hit == 1){
-    	unsigned int* mb_addr = sac->sets[setidx].ways[idx].block->data + addr_offt;
-    	*mb_addr = val;
-    	sac->sets[setidx].ways[idx].dirty = 1;
-    	mark_as_used(sac, setidx, idx);
+        unsigned int* mb_addr = sac->sets[setidx].ways[idx].block->data + addr_offt;
+        *mb_addr = val;
+        sac->sets[setidx].ways[idx].dirty = 1;
+        mark_as_used(sac, setidx, idx);
 
-    	++sac->cs.w_queries;
+        ++sac->cs.w_queries;
     }
     else{
-    	int lastw = lru(sac, setidx);
+        int lastw = lru(sac, setidx);
 
-    	if ((sac->sets[setidx].ways[lastw].valid == 1) && (sac->sets[setidx].ways[lastw].dirty == 1)){
-    		mm_write(sac->mm, sac->sets[setidx].ways[lastw].block->start_addr, sac->sets[setidx].ways[lastw].block);
-    	}
-    	memory_block* mb = mm_read(sac->mm, mb_start_addr);
-    	unsigned int* mb_addr = mb->data + addr_offt;
-    	*mb_addr = val;
-    	sac->sets[setidx].ways[lastw].block = mb;
-    	sac->sets[setidx].ways[lastw].valid = 1;
-    	sac->sets[setidx].ways[lastw].dirty = 1;
-    	mark_as_used(sac, setidx, lastw);
+        if ((sac->sets[setidx].ways[lastw].valid == 1) && (sac->sets[setidx].ways[lastw].dirty == 1)){
+            mm_write(sac->mm, sac->sets[setidx].ways[lastw].block->start_addr, sac->sets[setidx].ways[lastw].block);
+        }
+        memory_block* mb = mm_read(sac->mm, mb_start_addr);
+        unsigned int* mb_addr = mb->data + addr_offt;
+        *mb_addr = val;
+        sac->sets[setidx].ways[lastw].block = mb;
+        sac->sets[setidx].ways[lastw].valid = 1;
+        sac->sets[setidx].ways[lastw].dirty = 1;
+        mark_as_used(sac, setidx, lastw);
 
-    	++sac->cs.w_misses;
-    	++sac->cs.w_queries;
-    	// mb_free(mb);
+        ++sac->cs.w_misses;
+        ++sac->cs.w_queries;
+        // mb_free(mb);
     }
 }
 
@@ -142,38 +145,39 @@ unsigned int sac_load_word(set_associative_cache* sac, void* addr)
     int i;
     for(i = 0; i < SET_ASSOCIATIVE_NUM_WAYS; i++)
     {
+
         if((sac->sets[setidx].ways[i].valid == 1) && (sac->sets[setidx].ways[i].block->start_addr == mb_start_addr)){
-        	hit = 1;
-        	idx = i;
+            hit = 1;
+            idx = i;
         }
     }
 
     if (hit == 1){
-    	unsigned int* mb_addr = sac->sets[setidx].ways[idx].block->data + addr_offt;
-    	unsigned int result = *mb_addr;
-    	sac->sets[setidx].ways[idx].dirty = 1;
-    	mark_as_used(sac, setidx, idx);
-    	++sac->cs.r_queries;
-    	return result;
+        unsigned int* mb_addr = sac->sets[setidx].ways[idx].block->data + addr_offt;
+        unsigned int result = *mb_addr;
+        sac->sets[setidx].ways[idx].dirty = 0;
+        mark_as_used(sac, setidx, idx);
+        ++sac->cs.r_queries;
+        return result;
 
     }
     else{
-    	int lastw = lru(sac, setidx);
-    	if (sac->sets[setidx].ways[lastw].valid == 1 && sac->sets[setidx].ways[lastw].dirty == 1){
-    		mm_write(sac->mm, sac->sets[setidx].ways[lastw].block->start_addr, sac->sets[setidx].ways[lastw].block);
-    	}
-    	memory_block* mb = mm_read(sac->mm, mb_start_addr);
-    	unsigned int* mb_addr = mb->data + addr_offt;
-    	unsigned int result = *mb_addr;
-    	sac->sets[setidx].ways[lastw].block = mb;
-    	sac->sets[setidx].ways[lastw].valid = 1;
-    	sac->sets[setidx].ways[lastw].dirty = 0;
-    	mark_as_used(sac, setidx, lastw);
+        int lastw = lru(sac, setidx);
+        if (sac->sets[setidx].ways[lastw].valid == 1 && sac->sets[setidx].ways[lastw].dirty == 1){
+            mm_write(sac->mm, sac->sets[setidx].ways[lastw].block->start_addr, sac->sets[setidx].ways[lastw].block);
+        }
+        memory_block* mb = mm_read(sac->mm, mb_start_addr);
+        unsigned int* mb_addr = mb->data + addr_offt;
+        unsigned int result = *mb_addr;
+        sac->sets[setidx].ways[lastw].block = mb;
+        sac->sets[setidx].ways[lastw].valid = 1;
+        sac->sets[setidx].ways[lastw].dirty = 0;
+        mark_as_used(sac, setidx, lastw);
 
-    	++sac->cs.r_misses;
-    	++sac->cs.r_queries;
-    	// mb_free(mb);
-    	return result;
+        ++sac->cs.r_misses;
+        ++sac->cs.r_queries;
+        // mb_free(mb);
+        return result;
     }
 }
 
